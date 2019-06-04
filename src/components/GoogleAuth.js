4c20667a695e0a7352bdb6c98e196d9dbd2ffac4
Douglas de Jesus Costa <douglas.costa@squadra.com.br>
2019-06-04 16:43:50 -0300

    Complementing the authentication component

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-class GoogleAuth extends React.Component {
+const GoogleAuth = () => {
+  const [isSignedIn, setSignedIn] = useState(null);
 
-    componentDidMount() {
-        window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({
-                clientId: '110811983755-o9nplgfsksrhbif3krjn4mbmoqe3h6e2.apps.googleusercontent.com',
-                scope: 'email'
-            }).then(() => {
-                this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({isSignedIn: this.auth.isSignedIn.get()})
-            });
+  useEffect(() => {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "KEY",
+          scope: "email"
+        })
+        .then(() => {
+          let auth = window.gapi.auth2.getAuthInstance();
+          setSignedIn(auth.isSignedIn.get());
+
+          auth.isSignedIn.listen( () => {
+            setSignedIn(auth.isSignedIn.get())
+          })
         });
-    }
+    });
+  });
 
-    render() {
-        return <div>Google Auth</div>
-    }
-}
+  return (
+    <div>
+      {isSignedIn === null ? (
+        <div>I don't know if we are signed in</div>
+      ) : isSignedIn === true ? (
+        <div>I'm signed in</div>
+      ) : (
+        <div>I'm not signed in</div>
+      )}
+    </div>
+  );
+};
 
 export default GoogleAuth;
